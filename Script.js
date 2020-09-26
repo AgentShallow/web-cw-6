295bfed26e0522002235d1d804e17667a30897bc
AgentShallow <70897680+AgentShallow@users.noreply.github.com>
2020-09-26 21:33:18 +0300

    Done

---
+++ Script.js
@@ -0,0 +1,10 @@
+// let age=16;
+// console.log(age);
+// console.log(age*5)
+// console.log("حين أبلغ 5 أضعاف عمري سيبدأ زحل باستقبال المبرمجين على سطحه 🪐")
+// let numberofyears=2020-1969;
+// console.log(`هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر ${numberofyears}🌑`);
+let pets= ["Cat","Hamster","Turtle","Toucan","Kasko"];
+let Cat={name:"Babosa",age:14,emoji:"🐈",type:"Saimes"}
+console.log(pets)
+
